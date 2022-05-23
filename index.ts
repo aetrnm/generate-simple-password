5c53012c19145e7d3d416c75a35000a231d4d5f1
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-23 22:08:30 +0200

    refactor: options format

--- index.ts
+++ index.ts
@@ -2,23 +2,28 @@ const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowercase = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '0123456789';
 const specialChars = '~`!@#$%^&*()_-+={[}]|:;"\'<,>.?/\\';
+const defaultLength = 16;
 
-export default function generatePassword(
-  length: number = 16,
-  allowedChars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  options: {
-    addNumbers: boolean;
-    addSpecialChars: boolean;
-    customChars: string;
-  },
-) {
-  let result = '';
-  let characters = allowedChars;
+export default function generatePassword(options: {
+  length: number;
+  addUppercase: boolean;
+  addLowercase: boolean;
+  addNumbers: boolean;
+  addSpecialChars: boolean;
+  customChars: string;
+}) {
+  let characters = '';
+  const passwordLength = options.length ? options.length : defaultLength;
+
+  if (options && options.customChars) characters += options.customChars;
+  if (options && options.addUppercase) characters += uppercase;
+  if (options && options.addLowercase) characters += lowercase;
   if (options && options.addNumbers) characters += numbers;
   if (options && options.addSpecialChars) characters += specialChars;
-  if (options && options.customChars) characters += options.customChars;
-  const charactersLength = allowedChars.length;
-  for (let i = 0; i < length; i++) {
+
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < passwordLength; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
