1e8b659af98f1739504c40392f6f0cb255af1598
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-23 21:21:44 +0200

    feat: add custom chars

--- index.ts
+++ index.ts
@@ -4,14 +4,19 @@ const numbers = '0123456789';
 const specialChars = '~`!@#$%^&*()_-+={[}]|:;"\'<,>.?/\\';
 
 export default function generatePassword(
-  length: number,
+  length: number = 16,
   allowedChars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  options: { addNumbers: boolean; addSpecialChars: boolean },
+  options: {
+    addNumbers: boolean;
+    addSpecialChars: boolean;
+    customChars: string;
+  },
 ) {
   let result = '';
   let characters = allowedChars;
   if (options && options.addNumbers) characters += numbers;
   if (options && options.addSpecialChars) characters += specialChars;
+  if (options && options.customChars) characters += options.customChars;
   const charactersLength = allowedChars.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
