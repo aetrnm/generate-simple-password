e9a721a588a1a6eeeb78320a404cd7a6713f278e
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-24 21:55:08 +0200

    refactor: export default values to separate file

---
+++ defaults.ts
@@ -0,0 +1,9 @@
+enum DEFAULTS {
+  uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase = "abcdefghijklmnopqrstuvwxyz",
+  numbers = "0123456789",
+  specialChars = "~`!@#$%^&*()_-+={[}]|:;\"'<,>.?/\\",
+  defaultLength = 16,
+}
+
+export default DEFAULTS;
--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-const numbers = '0123456789';
-const specialChars = '~`!@#$%^&*()_-+={[}]|:;"\'<,>.?/\\';
-const defaultLength = 16;
+import DEFAULTS from "./defaults";
 
-export default function generatePassword(options: {
+interface PasswordOptions {
   length: number;
   addUppercase: boolean;
   addLowercase: boolean;
   addNumbers: boolean;
   addSpecialChars: boolean;
   customChars: string;
-}) {
-  let characters = '';
-  const passwordLength = options.length ? options.length : defaultLength;
+}
 
+export default function generatePassword(options: PasswordOptions) {
+  let characters = "";
+  const passwordLength = options.length
+    ? options.length
+    : DEFAULTS.defaultLength;
+
+  if (options && options.addUppercase) characters += DEFAULTS.uppercase;
+  if (options && options.addLowercase) characters += DEFAULTS.lowercase;
+  if (options && options.addNumbers) characters += DEFAULTS.numbers;
+  if (options && options.addSpecialChars) characters += DEFAULTS.specialChars;
   if (options && options.customChars) characters += options.customChars;
-  if (options && options.addUppercase) characters += uppercase;
-  if (options && options.addLowercase) characters += lowercase;
-  if (options && options.addNumbers) characters += numbers;
-  if (options && options.addSpecialChars) characters += specialChars;
 
-  let result = '';
+  let result = "";
   const charactersLength = characters.length;
   for (let i = 0; i < passwordLength; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
